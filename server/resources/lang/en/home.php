69f4873528b6d06a76e3cb57e4701dc767212b0a
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2021-04-06 10:56:48 +0200

    Add random local hospital to home page

--- server/resources/lang/en/home.php
+++ server/resources/lang/en/home.php
@@ -6,6 +6,7 @@ return [
 
     'auth_header' => 'Welcome :user.firstname!',
     'auth_description' => 'Go to the hospitals tab in the menu to view local hospitals and the trails they are running!',
+    'auth_local_hospital' => 'Your local hospital',
 
     'guest_header' => 'Welcome to the :app.name platform!',
     'guest_description' => 'Login or register to enroll in some vacin trails from hospitals near by!'
--- server/resources/views/home.blade.php
+++ server/resources/views/home.blade.php
@@ -5,10 +5,27 @@
 @section('content')
     <div class="content">
         @auth
-            <h1 class="title is-spaced">@lang('home.auth_header', ['user.firstname' => Auth::user()->firstname])</h1>
+            <h1 class="title">@lang('home.auth_header', ['user.firstname' => Auth::user()->firstname])</h1>
             <p>@lang('home.auth_description')</p>
+
+            <h2 class="subtitle">@lang('home.auth_local_hospital')</h2>
+
+            @php($hospitalId = session('random_hospital_id') ?? App\Models\Hospital::pluck('id')->random())
+            @php(session(['random_hospital_id' => $hospitalId]))
+            @php($hospital = App\Models\Hospital::find($hospitalId))
+            <div class="columns is-multiline">
+                <div class="column is-one-third">
+                    <div class="box content" style="height: 100%">
+                        <h2 class="title is-4">
+                            <a href="{{ route('hospitals.show', $hospital) }}">{{ $hospital->name }}</a>
+                        </h2>
+                        <p>{{ $hospital->address }}</p>
+                        <p>{{ $hospital->postcode }}, {{ $hospital->city }} {{ $hospital->country }}</p>
+                    </div>
+                </div>
+            </div>
         @else
-            <h1 class="title is-spaced">@lang('home.guest_header', ['app.name' => config('app.name')])</h1>
+            <h1 class="title">@lang('home.guest_header', ['app.name' => config('app.name')])</h1>
             <p>@lang('home.guest_description')</p>
         @endauth
     </div>
